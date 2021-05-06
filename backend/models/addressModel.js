0a49f5053cc10b379e7dce145595fb560af4f504
149311CB <an.nickhcm2000@gmail.com>
2021-05-06 16:30:50 +0700

    Update Models

--- backend/models/addressModel.js
+++ backend/models/addressModel.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const citySchema = mongoose.Schema({
+/* const citySchema = mongoose.Schema({
   name: {
     type: String,
     required: true
@@ -37,7 +37,46 @@ const wardSchema = mongoose.Schema({
     type: Date,
     required: true
   }
-})
+}) */
+
+/* const citySchema = mongoose.Schema({
+  Id: {
+    type: String,
+    required: false
+  },
+  Name: {
+    type: String,
+    required: true
+  },
+  Districts: [
+    {
+      Id: {
+        type: String,
+        required: true
+      },
+      Name: {
+        type: String,
+        required: true
+      },
+      Wards: [
+        {
+          Id: {
+            type: String,
+            required: true
+          },
+          Name: {
+            type: String,
+            required: true
+          },
+          Level: {
+            type: String,
+            required: true
+          }
+        }
+      ]
+    }
+  ]
+}) */
 
 const addressSchema = mongoose.Schema({
   receiverFirstName: {
@@ -48,9 +87,18 @@ const addressSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  city: [citySchema],
-  district: [districtSchema],
-  ward: [wardSchema],
+  city: {
+    type: String,
+    required: true
+  },
+  district: {
+    type: String,
+    required: true
+  },
+  ward: {
+    type: String,
+    required: true
+  },
   addressDetails: {
     type: String,
     required: true
